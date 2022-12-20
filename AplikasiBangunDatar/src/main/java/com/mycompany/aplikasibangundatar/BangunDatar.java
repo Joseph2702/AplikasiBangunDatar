4c204903dcbde814271da64a9e5f0f53481b887d
Joseph2702 <joseph.frinando@mhs.itenas.ac.id>
2022-12-20 07:54:50 +0700

    initate commit project

---
+++ AplikasiBangunDatar/src/main/java/com/mycompany/aplikasibangundatar/BangunDatar.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.aplikasibangundatar;
+
+/**
+ *
+ * @author DELL
+ */
+abstract class BangunDatar {
+
+    private String namaBangunDatar;
+
+    public BangunDatar() {
+    }
+
+    public BangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+
+    public String getNamaBangunDatar() {
+        return namaBangunDatar;
+    }
+
+    public void setNamaBangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+    
+    abstract double luas();
+    abstract double keliling();
+}
